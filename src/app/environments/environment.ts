b79114c43ad1bb8ec3f816023b93ec40b8922d25
Emmanuel Sanchez Velasquez <u20216260@utp.edu.pe>
2025-12-08 12:16:56 -0500

    Actualizacion de dominio

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   //   https://backend-melamina.onrender.com/api (dominio para despliegue)
   //   http://localhost:8080/api (dominio para desarrollo)
-  apiBaseUrl: 'http://localhost:8080/api',
+  apiBaseUrl: 'https://backend-melamina.onrender.com/api',
 };
