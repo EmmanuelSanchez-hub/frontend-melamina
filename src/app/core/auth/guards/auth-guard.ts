385c40196ef2d03f9170570d58d63b80af248f88
Emmanuel Sanchez Velasquez <u20216260@utp.edu.pe>
2025-12-08 20:24:29 -0500

    Fix: se restringe acceso de ROLE_CLIENTE en authGuard

--- src/app/core/auth/guards/auth-guard.ts
+++ src/app/core/auth/guards/auth-guard.ts
@@ -5,9 +5,17 @@ export const authGuard: CanActivateFn = (route, state) => {
   const router = inject(Router);
 
   const token = localStorage.getItem('token');
+  const user = JSON.parse(localStorage.getItem('user') || '{}');
 
   if (!token) {
     router.navigate(['/login']);
+    return false;
+  }
+
+  const rol = user.rol;
+
+  if (rol === 'ROLE_CLIENTE' && state.url.startsWith('/app')) {
+    router.navigate(['/inicio']);
     return false;
   }
 
